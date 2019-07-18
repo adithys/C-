3d5a5e1d09c7c142a0092cf6cba9185f6c4f239a
adithys <51487891+adithys@users.noreply.github.com>
2019-07-18 10:21:05 +0400

    Update C-task 12 -temperature conversion.cpp

--- C-task 12 -temperature conversion.cpp
+++ C-task 12 -temperature conversion.cpp
@@ -1,24 +1,34 @@
 //Adithya Krishnan
 // C PROGRAM Task 12- TO DISPLAY THE FOLLOWING OPTIONS : 1. CELCIUS TO FAHRENHEIT, 2. FAHRENHEIT TO CELCIUS, 3. EXIT. PERFORM THE RELEVANT OPERATION USING SWITCH CASE.
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
 	int choice;
+	char ch='Y';
 	float fahr,celc;
-	printf("Menu\n1.Celsius to fahrenheit\n2.fahrenheit to celsius\nEnter ur appropriate choice");
-	scanf("%d",&choice);
-	switch(choice)
-	  {
-	  	case 1: printf("Celsius to fahrenheit");
-	  	        printf("\nEnter the temperature in celsius:");
-	  	        scanf("%f",&celc);
-	  	        fahr=(celc*1.8)+32.0;
-	  	        printf("Temperature in fahrenheit: %f",fahr);
-	  	        break;
-	  	case 2: printf("Fahrenheit to celsius");
-	  	        printf("\nEnter the temperature in fahrenheit:");
-	  	        scanf("%f",&fahr);
-	  	        celc=(fahr-32.0)/1.8;
-	  	        printf("Temperature in celsius: %f",celc);  		       
-	  }
+	do
+	{	
+	  printf("\n\nMenu\n1.Celsius to fahrenheit\n2.fahrenheit to celsius\nEnter ur appropriate choice ");
+	  scanf("%d",&choice);
+	  switch(choice)
+	    {
+	        	case 1: printf("Celsius to fahrenheit");
+	  	                printf("\nEnter the temperature in celsius:");
+	  	                scanf("%f",&celc);
+	  	                fahr=(celc*1.8)+32.0;
+	  	                printf("Temperature in fahrenheit: %f",fahr);
+	  	                break;
+	  	        case 2: printf("Fahrenheit to celsius");
+	  	                printf("\nEnter the temperature in fahrenheit:");
+	  	                scanf("%f",&fahr);
+	  	                celc=(fahr-32.0)/1.8;
+	  	                printf("Temperature in celsius: %f",celc);
+			        break;
+		        case 3: exit(0);	
+	    }
+		
+         }while(ch=='y'||ch=='Y');
+
+	
 }
